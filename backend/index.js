4227abe07545d16ff0ae3a57b162ecf25e2d0dc4
Aarya Kasbe <aaryakasbe05@gmail.com>
2025-04-05 16:03:40 +0530

    backend setup

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,9 @@ require('dotenv').config();
 const app=express();
 const port=process.env.PORT;
 
+app.get("/",(req,res)=>{
+    res.send("Hello!");
+})
 
 app.listen(port,(req,res)=>{
     console.log(`Server listening on port ${port}`);
